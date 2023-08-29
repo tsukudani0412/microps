09bc574e3d6355d35b215c32ac74004da67c1e27
Tsukudani <me@tkdn.jp>
2023-08-29 19:01:41 +0900

    Step 21: Add event driven interrupt

--- platform/linux/intr.c
+++ platform/linux/intr.c
@@ -108,6 +108,9 @@ intr_thread(void *arg)
     case SIGUSR1:
       net_softirq_handler();
       break;
+    case SIGUSR2:
+      net_event_handler();
+      break;
     case SIGALRM:
       net_timer_handler();
       break;
@@ -165,6 +168,7 @@ intr_init(void)
   sigemptyset(&sigmask);
   sigaddset(&sigmask, SIGHUP);
   sigaddset(&sigmask, SIGUSR1);
+  sigaddset(&sigmask, SIGUSR2);
   sigaddset(&sigmask, SIGALRM);
   return 0;
 }
--- udp.c
+++ udp.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <errno.h>
 
 #include "platform.h"
 
@@ -40,7 +41,7 @@ struct udp_pcb {
   int state;
   struct ip_endpoint local;
   struct queue_head queue; /* receive queue */
-  int wc; /*wait count */
+  struct sched_ctx ctx;
 };
 
 struct udp_queue_entry {
@@ -81,6 +82,7 @@ udp_pcb_alloc(void)
   for(pcb = pcbs; pcb < tailof(pcbs); pcb++) {
     if(pcb->state == UDP_PCB_STATE_FREE) {
       pcb->state = UDP_PCB_STATE_OPEN;
+      sched_ctx_init(&pcb->ctx);
       return pcb;
     }
   }
@@ -92,8 +94,9 @@ udp_pcb_release(struct udp_pcb *pcb)
 {
   struct queue_entry *entry;
 
-  if(pcb->wc) {
-    pcb->state = UDP_PCB_STATE_CLOSING;
+  pcb->state = UDP_PCB_STATE_CLOSING;
+  if(sched_ctx_destroy(&pcb->ctx) == -1) {
+    sched_wakeup(&pcb->ctx);
     return;
   }
   pcb->state = UDP_PCB_STATE_FREE;
@@ -201,6 +204,7 @@ udp_input(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, struct 
   memcpy(entry + 1, hdr + 1, entry->len);
   queue_push(&pcb->queue, entry);
   debugf("queue pushed: id=%d, num=%d", udp_pcb_id(pcb), pcb->queue.num);
+  sched_wakeup(&pcb->ctx);
   mutex_unlock(&mutex);
 }
 
@@ -244,11 +248,30 @@ udp_output(struct ip_endpoint *src, struct ip_endpoint *dst, const uint8_t *data
   return len;
 }
 
+static void 
+event_handler(void *arg)
+{
+  struct udp_pcb *pcb;
+
+  (void)arg;
+  mutex_lock(&mutex);
+  for(pcb = pcbs; pcb < tailof(pcbs); pcb++) {
+    if(pcb->state == UDP_PCB_STATE_OPEN) {
+      sched_interrupt(&pcb->ctx);
+    }
+  }
+  mutex_unlock(&mutex);
+}
+
 int 
 udp_init(void)
 {
  if(ip_protocol_register(IP_PROTOCOL_UDP, udp_input) == -1) {
    errorf("ip_protocol_register() failure");
+   return -1;
+ }
+ if(net_event_subscribe(event_handler, NULL) == -1) {
+   errorf("net_event_subscribe() failure");
    return -1;
  }
  return 0;
@@ -294,7 +317,6 @@ int udp_bind(int id, struct ip_endpoint *local)
 {
   struct udp_pcb *pcb, *exist;
   char ep1[IP_ENDPOINT_STR_LEN];
-  char ep2[IP_ENDPOINT_STR_LEN];
 
   mutex_lock(&mutex);
   pcb = udp_pcb_get(id);
@@ -370,6 +392,7 @@ udp_recvfrom(int id, uint8_t *buf, size_t size, struct ip_endpoint *foreign)
   struct udp_pcb *pcb;
   struct udp_queue_entry *entry;
   ssize_t len;
+  int err;
 
   mutex_lock(&mutex);
   pcb = udp_pcb_get(id);
@@ -383,11 +406,14 @@ udp_recvfrom(int id, uint8_t *buf, size_t size, struct ip_endpoint *foreign)
     if(entry) {
       break;
     }
-    pcb->wc++; //pcb in use flag
-    mutex_unlock(&mutex);
-    sleep(1);
-    mutex_lock(&mutex);
-    pcb->wc--;
+    /* wait to be woken up by sched_wakeup() or sched_interrupt() */
+    err = sched_sleep(&pcb->ctx, &mutex, NULL);
+    if(err) {
+      debugf("interrupted");
+      mutex_unlock(&mutex);
+      errno = EINTR;
+      return -1;
+    }
     if(pcb->state == UDP_PCB_STATE_CLOSING) {
       debugf("closed");
       udp_pcb_release(pcb);
